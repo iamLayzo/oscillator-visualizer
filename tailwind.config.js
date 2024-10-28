39aebc98c8e844125255b670c6903f16b10143a8
iamLayzo <jesusbclock@gmail.com>
2024-10-28 00:35:36 -0300

    Setup project configuration and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // Asegúrate de incluir archivos .tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
